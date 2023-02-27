031bfb3393a53091ddd8a9e03472378c10a51dce
Hana Oh <108045487+hanaisreal@users.noreply.github.com>
2023-02-27 14:15:35 +0900

    Time: 9 ms (96.44%), Space: 42.3 MB (27.36%) - LeetHub

--- Easy/0009-palindrome-number/0009-palindrome-number.java
+++ Easy/0009-palindrome-number/0009-palindrome-number.java
@@ -1,22 +1,17 @@
 class Solution {
     public boolean isPalindrome(int x) {
 
-        //if the number is negative, it is not a palindrome
-        if(x < 0){
-            return false;
-        }
-        if( x == 0) {
-            return true;
+        if(x < 0) {
+	        return false;
         }
 
-        String num = Integer.toString(x);
+        int y = x;
+        int z = 0;  
+        while(y > 0){
+            z = z * 10 + y % 10;  //don't reverse the num!
+            y = y / 10;
+        }	
 
-        for(int i = 0, j = num.length()-1; i < num.length()/2; i++, j--){
-            if(num.charAt(i) != num.charAt(j)){
-                return false;
-            }
-        }
-
-        return true;
+        return x == z;
     }
 }
