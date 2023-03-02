6810a65c5ab4d0a109338c14a62c641d384a7393
Hana Oh <108045487+hanaisreal@users.noreply.github.com>
2023-03-03 00:06:55 +0900

    Time: 1 ms (98.42%), Space: 42.3 MB (29.22%) - LeetHub

--- Medium/0443-string-compression/0443-string-compression.java
+++ Medium/0443-string-compression/0443-string-compression.java
@@ -7,22 +7,24 @@ class Solution {
         
         if(n==1)
             return 1;
+        
         while(i<n){
             int count=1;
             char charac=chars[i];
-            while(i+1<n && chars[i]==chars[i+1]){
+            while(i+1<n && chars[i]==chars[i+1]){  //continue until the numbers aren't duplicated
                 count++;
                 i++;
             }
-            if(count==1){
-                chars[j++]=charac;
+            
+            if(count==1){  //if the duplication is 1, the next char would be the new char
+                chars[j++]=charac; //cover the previous array with new one. 
             }
             else{
                 if(count>1){
                     chars[j++]=charac;
                     String counterstring=count+"";
-                    for(int r=0;r<counterstring.length();r++){
-                        chars[j++]=counterstring.charAt(r);
+                    for(int r=0;r<counterstring.length();r++){  //incase the count is over 1 digit,
+                        chars[j++]=counterstring.charAt(r);  //change the count to string and count digit. 
                     }
                 }
             }
