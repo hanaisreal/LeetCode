aadd9bb323d17b066cf66fe373666fddadf02e00
Hana Oh <108045487+hanaisreal@users.noreply.github.com>
2023-03-03 13:03:33 +0900

    Time: 1 ms (98.42%), Space: 42.5 MB (28.51%) - LeetHub

--- Medium/0443-string-compression/0443-string-compression.java
+++ Medium/0443-string-compression/0443-string-compression.java
@@ -2,33 +2,33 @@ class Solution {
     public int compress(char[] chars) {
         //if the previous char is the same as the current char, increase count
         //two pointers, count duplicate, if it exceeds 1, then it is compressed
-        int i = 0,j=0;
-        int n=chars.length;
         
-        if(n==1)
-            return 1;
+        int i = 0; 
+        int j = 0;
+        int length = chars.length;
         
-        while(i<n){
-            int count=1;
-            char charac=chars[i];
-            while(i+1<n && chars[i]==chars[i+1]){  //continue until the numbers aren't duplicated
-                count++;
+        if(length == 1) return 1;
+        
+        while(i < length){
+            int count = 1;
+            char charac = chars[i];
+            
+            while(i < length-1 && chars[i] == chars[i+1]){  //continue until the numbers aren't duplicated
                 i++;
+                count++;
             }
             
-            if(count==1){  //if the duplication is 1, the next char would be the new char
-                chars[j++]=charac; //cover the previous array with new one. 
-            }
-            else{
-                if(count>1){
-                    chars[j++]=charac;
-                    String counterstring=count+"";
-                    for(int r=0;r<counterstring.length();r++){  //incase the count is over 1 digit,
-                        chars[j++]=counterstring.charAt(r);  //change the count to string and count digit. 
-                    }
+            chars[j++] = charac;
+            if(count == 1){ //if there is no duplication, no need to add numbers
+                i++;
+                continue;
+            }else{
+                String countString = count + "";
+                for(int k = 0; k < countString.length(); k++){  //in case the count is over 1 digit 
+                    chars[j++] = countString.charAt(k);  //change the count to string and add number
                 }
             }
-            i++;
+            i++; //!! need this so that the next new char is save to charac for comparison
         }
         return j;
     }
