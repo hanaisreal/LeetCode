4713fa0e99a44800ea287980a3da0dc61430ede9
Hana Oh <108045487+hanaisreal@users.noreply.github.com>
2023-03-08 00:39:23 +0900

    Time: 380 ms (25.92%), Space: 55.6 MB (91.07%) - LeetHub

--- Medium/2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.java
+++ Medium/2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.java
@@ -1,32 +1,18 @@
 class Solution {
     public long minimumTime(int[] time, int totalTrips) {
-        
-        long low = Long.MAX_VALUE;
-        long high = 0;
-        for(int it : time){ //finding min num
-            low = Math.min(low,it);
-        }
-        high = totalTrips*low;  //high would be the maximum time taken for all low to be considered. 
-        
-        while(low < high){
-            long mid = low + (high - low) / 2;
-            
-            if(numTrips(mid,totalTrips,time)){
-                high = mid;
+        long lo = 0, hi = 100_000_000_000_000L;
+        while (lo < hi) {
+            long need = lo + (hi - lo) / 2;
+            long trips = 0;
+            for (int t : time) {
+                trips += need / t;
             }
-            else
-                low = mid + 1;
+            if (trips < totalTrips) {
+                lo = need + 1;
+            }else {
+                hi = need;
+            }
         }
-        return low;
-    }
-    public boolean numTrips(long isvalidtime,int totalTrips,int[] time){
-        long trips = 0;
-        
-        for(int it : time){
-            trips += isvalidtime / it;
-        }
-        if(trips >= totalTrips)
-            return true;
-        return false;
+        return lo;
     }
 }
