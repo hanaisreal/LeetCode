ec7bd846f8105755aca5dffbf71e495f4ee8710b
Hana Oh <108045487+hanaisreal@users.noreply.github.com>
2023-03-08 00:44:46 +0900

    Time: 182 ms (94.77%), Space: 57.4 MB (16.34%) - LeetHub

--- Medium/2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.java
+++ Medium/2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.java
@@ -1,39 +1,22 @@
 class Solution {
     public long minimumTime(int[] time, int totalTrips) {
-        int mx = 0, min = time[0];
-        for(int t : time){
-            mx = Math.max(t, mx);
-            min = Math.min(t, min);
-        }
-        long totalAtMx = 0;
-        for(int t : time){
-            totalAtMx += mx/t;
-        }
-        
-        long k = totalTrips/totalAtMx;
-        long remainder = totalTrips%totalAtMx;
-        if(remainder != 0) k++;
-        
-        
-
-        long l = min, r = mx*k;
-        while(l <= r){
-            long mid = l + (r-l)/2;
-            long currTotal = getTotal(time, mid);
-            if(currTotal < totalTrips){
-                l = mid+1;
-            }else{
-                r = mid-1;
+        int n = time.length;
+        long left = 1, right = (long)1e18;
+        while (left < right) {
+            long mid = left + (right - left) / 2;
+            long trips = 0;
+            for (int i = 0; i < n; i++) {
+                trips += mid / time[i];
+                if (trips >= totalTrips) {
+                    break;
+                }
+            }
+            if (trips >= totalTrips) {
+                right = mid;
+            } else {
+                left = mid + 1;
             }
         }
-        return l;
-    }
-    
-    int getTotal(int[] time, long budget){
-        int ret = 0;
-        for(int t : time){
-            ret += (budget/t);
-        }
-        return ret;
+        return left;
     }
 }
