b662b469b8cc1c67641fe6c086bf885ec358ace4
Hana Oh <108045487+hanaisreal@users.noreply.github.com>
2023-03-19 18:23:32 +0900

    Time: 797 ms (50.79%), Space: 100.1 MB (41.56%) - LeetHub

--- Medium/0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
+++ Medium/0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
@@ -1,69 +1,34 @@
-class TrieNode {
-    Map<Character, TrieNode> children;
-    boolean isWord;
-    
-    public TrieNode() {
-        children = new HashMap<>();
-        isWord = false;
-    }
-}
-
 class WordDictionary {
-    private TrieNode root;
+    private WordDictionary[] children;
+    boolean isCompleteWord;
 
     public WordDictionary() {
-        root = new TrieNode();
+        children = new WordDictionary[26];
+        isCompleteWord = false;
     }
-
+    
     public void addWord(String word) {
-        TrieNode node = root;
-        // Traverse the trie for each character in the word
-        for (int i = 0; i < word.length(); i++) {
-            char c = word.charAt(i);
-            // If the current node does not have a child with character c,
-            // create a new node and add it as a child of the current node
-            if (!node.children.containsKey(c)) {
-                node.children.put(c, new TrieNode());
-            }
-            // Move to the child node corresponding to character c
-            node = node.children.get(c);
+        WordDictionary curr = this;
+        for(char c: word.toCharArray()){
+            if(curr.children[c - 'a'] == null)
+                curr.children[c - 'a'] = new WordDictionary();
+            curr = curr.children[c - 'a'];
         }
-        // Mark the last node as a word node
-        node.isWord = true;
+        curr.isCompleteWord = true;
     }
-
+    
     public boolean search(String word) {
-        return searchHelper(root, word, 0);
-    }
-
-    private boolean searchHelper(TrieNode node, String word, int index) {
-        // If we have reached the end of the word,
-        // check if the current node is a word node
-        if (index == word.length()) {
-            return node.isWord;
-        }
-        char c = word.charAt(index);
-        if (c == '.') {
-            // If the current character is a dot, we need to check all children of the current node
-            // recursively by skipping over the dot character and moving to the next character of the word
-            for (TrieNode child : node.children.values()) {
-                if (searchHelper(child, word, index + 1)) {
-                    return true;
-                }
-            }
-            // If no child node matches the remaining characters of the word,
-            // return false
-            return false;
-        } else {
-            // If the current character is not a dot, move to the child node
-            // corresponding to that character and continue recursively
-            TrieNode child = node.children.get(c);
-            if (child == null) {
-                // If there is no child node corresponding to the current character,
-                // return false
+        WordDictionary curr = this;
+        for(int i = 0; i < word.length(); ++i){
+            char c = word.charAt(i);
+            if(c == '.'){
+                for(WordDictionary ch: curr.children)
+                    if(ch != null && ch.search(word.substring(i+1))) return true;
                 return false;
             }
-            return searchHelper(child, word, index + 1);
+            if(curr.children[c - 'a'] == null) return false;
+            curr = curr.children[c - 'a'];
         }
+        return curr != null && curr.isCompleteWord;
     }
 }
