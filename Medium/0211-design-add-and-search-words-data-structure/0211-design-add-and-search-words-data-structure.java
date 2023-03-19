1913779ba853b734c74b86c4f18f82d8199e9aa7
Hana Oh <108045487+hanaisreal@users.noreply.github.com>
2023-03-19 18:24:05 +0900

    Time: 420 ms (87.70%), Space: 93.5 MB (64.71%) - LeetHub

--- Medium/0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
+++ Medium/0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
@@ -1,34 +1,57 @@
-class WordDictionary {
-    private WordDictionary[] children;
-    boolean isCompleteWord;
+class TrieNode {
+    boolean isEndOfWord;
+    TrieNode[] children;
 
-    public WordDictionary() {
-        children = new WordDictionary[26];
-        isCompleteWord = false;
-    }
-    
-    public void addWord(String word) {
-        WordDictionary curr = this;
-        for(char c: word.toCharArray()){
-            if(curr.children[c - 'a'] == null)
-                curr.children[c - 'a'] = new WordDictionary();
-            curr = curr.children[c - 'a'];
-        }
-        curr.isCompleteWord = true;
-    }
-    
-    public boolean search(String word) {
-        WordDictionary curr = this;
-        for(int i = 0; i < word.length(); ++i){
-            char c = word.charAt(i);
-            if(c == '.'){
-                for(WordDictionary ch: curr.children)
-                    if(ch != null && ch.search(word.substring(i+1))) return true;
-                return false;
-            }
-            if(curr.children[c - 'a'] == null) return false;
-            curr = curr.children[c - 'a'];
-        }
-        return curr != null && curr.isCompleteWord;
+    public TrieNode() {
+        isEndOfWord = false;
+        children = new TrieNode[26];
     }
 }
+
+class WordDictionary {
+    private TrieNode root;
+
+    public WordDictionary() {
+        root = new TrieNode();
+    }
+
+    public void addWord(String word) {
+        TrieNode curr = root;
+        for (char c : word.toCharArray()) {
+            if (curr.children[c - 'a'] == null) {
+                curr.children[c - 'a'] = new TrieNode();
+            }
+            curr = curr.children[c - 'a'];
+        }
+        curr.isEndOfWord = true;
+    }
+
+    public boolean search(String word) {
+        return searchHelper(word, 0, root);
+    }
+
+    private boolean searchHelper(String word, int index, TrieNode curr) {
+        if (index == word.length()) {
+            return curr.isEndOfWord;
+        }
+
+        char c = word.charAt(index);
+
+        if (c == '.') {
+            for (TrieNode child : curr.children) {
+                if (child != null && searchHelper(word, index + 1, child)) {
+                    return true;
+                }
+            }
+            return false;
+        } else {
+            TrieNode child = curr.children[c - 'a'];
+            if (child == null) {
+                return false;
+            }
+            return searchHelper(word, index + 1, child);
+        }
+    }
+}
+
+
