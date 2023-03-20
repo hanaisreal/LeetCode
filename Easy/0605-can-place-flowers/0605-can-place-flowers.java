e42aa9f1b3e724a3ef8e74c1ee4ee4f05563bd2d
Hana Oh <108045487+hanaisreal@users.noreply.github.com>
2023-03-20 17:11:31 +0900

    Time: 1 ms (100.00%), Space: 43.8 MB (31.73%) - LeetHub

--- Easy/0605-can-place-flowers/0605-can-place-flowers.java
+++ Easy/0605-can-place-flowers/0605-can-place-flowers.java
@@ -1,23 +1,21 @@
-public class Solution {
+class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
         int count = 0;
-        for (int i = 0; i < flowerbed.length; i++) {
-            // Check if the current plot is empty.
-            if (flowerbed[i] == 0) {
-                // Check if the left and right plots are empty.
-                boolean emptyLeftPlot = (i == 0) || (flowerbed[i - 1] == 0);
-                boolean emptyRightPlot = (i == flowerbed.length - 1) || (flowerbed[i + 1] == 0);
+        
+        for(int i=0; i<flowerbed.length; i++){
+            if(flowerbed[i] == 0){
+                int prev = (i==0 || flowerbed[i-1]==0) ? 0 : 1;
+                int next = (i==flowerbed.length-1 || flowerbed[i+1]==0)  ? 0 : 1;
                 
-                // If both plots are empty, we can plant a flower here.
-                if (emptyLeftPlot && emptyRightPlot) {
+                if(prev==0 && next==0){
                     flowerbed[i] = 1;
                     count++;
-                    if (count >= n) {
-                        return true;
-                    }
                 }
             }
+            if(count >= n){
+                return true;
+            }
         }
-        return count >= n;
+        return false;
     }
 }
