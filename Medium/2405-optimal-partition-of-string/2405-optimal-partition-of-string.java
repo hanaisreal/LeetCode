30a3087606fb42fff304581833cfdc239426a510
Hana Oh <108045487+hanaisreal@users.noreply.github.com>
2023-04-05 08:29:18 +0900

    Time: 8 ms (95.41%), Space: 43.1 MB (67.78%) - LeetHub

--- Medium/2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
+++ Medium/2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
@@ -1,23 +1,37 @@
 class Solution {
+//     public int partitionString(String s) {
+//         //hashmap approach
+//         //iterate through the array, store letter if it is not stored in hashmap
+//         //if the letter repeats, increase the count and clear the hashmap
+        
+//         int idx = 0;
+//         int count = 0;
+//         Map<Character,Boolean> map = new HashMap<>();
+        
+//         while( idx < s.length()){
+//             if(map.containsKey(s.charAt(idx))){
+//                 count++;
+//                 map.clear(); //clear map to start a new substring
+//             }
+//             map.put(s.charAt(idx), true); //mark current character as seen
+//             idx++;
+//         }
+        
+//         return count + 1; //+1 because we need to count the last substring
+        
+//     }
+    
     public int partitionString(String s) {
-        //hashmap approach
-        //iterate through the array, store letter if it is not stored in hashmap
-        //if the letter repeats, increase the count and clear the hashmap
-        
-        int idx = 0;
-        int count = 0;
-        Map<Character,Boolean> map = new HashMap<>();
-        
-        while( idx < s.length()){
-            if(map.containsKey(s.charAt(idx))){
-                count++;
-                map.clear(); //clear map to start a new substring
+        int i = 0, ans = 1, flag = 0;
+        while(i < s.length()) {
+            int val = s.charAt(i) - 'a';
+            if ((flag & (1 << val)) != 0) {
+                flag = 0;
+                ans++;
             }
-            map.put(s.charAt(idx), true); //mark current character as seen
-            idx++;
+            flag = flag | (1 << val);
+            i++;
         }
-        
-        return count + 1; //+1 because we need to count the last substring
-        
+        return ans;
     }
 }
