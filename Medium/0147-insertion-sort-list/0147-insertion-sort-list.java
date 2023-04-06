660895eaaec0ea67ba58044fb0d82feb4a070734
Hana Oh <108045487+hanaisreal@users.noreply.github.com>
2023-04-07 00:22:24 +0900

    Time: 3 ms (87.99%), Space: 42.5 MB (18.60%) - LeetHub

---
+++ Medium/0147-insertion-sort-list/0147-insertion-sort-list.java
@@ -0,0 +1,27 @@
+class Solution {
+    public ListNode insertionSortList(ListNode head) {
+              ListNode d = new ListNode(0, head);
+        
+        ListNode prev = head;
+        ListNode curr = head.next;
+        
+        while (curr != null) {
+            if (curr.val >= prev.val) {
+                prev = curr;
+                curr = curr.next;
+                continue;
+            }
+            
+            ListNode tmp = d;
+            while (curr.val > tmp.next.val) {
+                tmp = tmp.next;
+            }
+            
+            prev.next = curr.next;
+            curr.next = tmp.next;
+            tmp.next = curr;
+            curr = prev.next;
+        }
+        return d.next;
+    }
+}
