1013375fb76f1cc3abdd3488da73cc4abdeff565
Hana Oh <108045487+hanaisreal@users.noreply.github.com>
2023-04-10 08:09:29 +0900

    Time: 94 ms (60.71%), Space: 94.7 MB (91.96%) - LeetHub

---
+++ Hard/1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int largestPathValue(String colors, int[][] edges) {
+        //topological sort: iterate over all of its paths in an optimal manner
+        
+        int n = colors.length();
+        Map<Integer, List<Integer>> adj = new HashMap<>();
+        int[] indegree = new int[n];
+        
+        
+        for(int[] edge: edges){
+            adj.computeIfAbsent(edge[0], k-> new ArrayList<Integer>()).add(edge[1]);
+            indegree[edge[1]]++;
+        }
+        
+        //create 2D-array count with n rows and 26 columns
+        int[][] count = new int[n][26];
+        Queue<Integer> q = new LinkedList<>();
+        
+        //push all the nodes with indegree zero in the queue
+        for(int i = 0; i < n; i++){
+            if(indegree[i] == 0){
+                q.offer(i);
+            }
+        }
+        
+        int answer = 1, nodesSeen = 0;
+        while(!q.isEmpty()){
+            int node = q.poll();
+            answer = Math.max(answer, ++count[node][colors.charAt(node) - 'a']);
+            nodesSeen++;
+            
+            if(!adj.containsKey(node)){
+                continue;
+            }
+            
+            for(int neighbor: adj.get(node)){
+                for(int i = 0; i < 26; i++){
+                    //try to update the frequency of colors for the neighbor to include paths that use node -> neighbor edge
+                    count[neighbor][i] = Math.max(count[neighbor][i], count[node][i]);
+                }
+                
+                indegree[neighbor]--;
+                if(indegree[neighbor] == 0){
+                    q.offer(neighbor);
+                }
+            }
+            
+        }
+        return nodesSeen < n ? -1: answer;
+        
+    }
+}
