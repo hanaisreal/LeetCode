265979268c408c38364b5e1deb99c47752bb110e
Hana Oh <108045487+hanaisreal@users.noreply.github.com>
2023-04-18 08:20:34 +0900

    Time: 1 ms (81.10%), Space: 42.8 MB (35.08%) - LeetHub

---
+++ Easy/1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        
+        List<Boolean> result = new LinkedList<>();
+        
+        //greedy approach, check if candies[i] + extraCandies >= maximum in candies[i]
+        
+        //find max  number of candies
+        int max = -1;
+        for(int num: candies){
+            if(num >= max) {
+                max = num;
+            }
+        }
+        
+        for(int candy: candies){
+            if(candy + extraCandies >= max){
+                result.add(true);
+            }else{
+                result.add(false);
+            }
+        }
+        return result;
+    }
+}
