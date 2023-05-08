d88d6474ca4b908c28cdf7b30bde327917c7e41d
Hana Oh <108045487+hanaisreal@users.noreply.github.com>
2023-05-08 09:04:05 +0900

    Time: 97 ms (23.88%), Space: 56.1 MB (29.85%) - LeetHub

--- Hard/1964-find-the-longest-valid-obstacle-course-at-each-position/1964-find-the-longest-valid-obstacle-course-at-each-position.java
+++ Hard/1964-find-the-longest-valid-obstacle-course-at-each-position/1964-find-the-longest-valid-obstacle-course-at-each-position.java
@@ -1,23 +1,36 @@
 class Solution {
     public int[] longestObstacleCourseAtEachPosition(int[] obstacles) {
         int n = obstacles.length;
-        int length = 0;
-        int[] result = new int[n];
-        int[] increasingSubseq = new int[n];
-        for (int i = 0; i < n; ++i) {
-            int left = 0, right = length;
-            while (left < right) {
-                int mid = (left + right) / 2;
-                if (increasingSubseq[mid] <= obstacles[i])
-                    left = mid + 1;
-                else
-                    right = mid;
+        int[] dp = new int[n];  // stores the smallest ending number of an increasing subsequence
+        int[] ans = new int[n]; // stores the length of the longest increasing subsequence that includes each obstacle
+        
+        int len = 0; // length of the longest increasing subsequence seen so far
+        
+        for (int i = 0; i < n; i++) {
+            int idx = binarySearch(dp, 0, len - 1, obstacles[i]); // find the position where we can add the current obstacle
+            
+            dp[idx] = obstacles[i]; // add the current obstacle to the dp array at the correct position
+            
+            if (idx == len) { // if we added the current obstacle to the end of the dp array
+                len++; // update the length of the longest increasing subsequence seen so far
             }
-            result[i] = left + 1;
-            if (length == left)
-                length++;
-            increasingSubseq[left] = obstacles[i];
+            
+            ans[i] = idx + 1; // update the length of the longest increasing subsequence that includes the i-th obstacle
         }
-        return result;
+        
+        return ans;
+    }
+    
+    // binary search to find the position where we can add the current obstacle in the dp array
+    private int binarySearch(int[] dp, int left, int right, int target) {
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (dp[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
     }
 }
