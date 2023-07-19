f7171fcebfca501f7947bab95f74186d95cb34ff
Hana Oh <108045487+hanaisreal@users.noreply.github.com>
2023-07-19 12:04:37 +0900

    Time: 72 ms (52.30%), Space: 96.7 MB (74.01%) - LeetHub

--- Medium/0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ Medium/0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -6,17 +6,18 @@ class Solution {
         //return result
         
         int n = intervals.length;
-        Arrays.sort(intervals, (a,b) -> Integer.compare(a[1], b[1]));
+        Arrays.sort(intervals, (a,b) -> Integer.compare(a[1], b[1])); //sort based on end times in ascending order. to prioritize meetings that end early
         
         int prev = 0;
-        int count = 1;
+        int count = 1; //store number of non-overlapping meetings found so far. 
         
-        for(int i = 1; i < n; i++){
-            if(intervals[i][0] >= intervals[prev][1]) {
+        for(int i = 1; i < n; i++){ 
+            if(intervals[i][0] >= intervals[prev][1]) {  //if the start time of the current interval is greater than or equal to the end time of the previous interval, it means the current interval does not overlap with the previous one. Thus, we can update prev to the current index i and increment count.
                 prev = i;
                 count++;
             }
         }
+        
         return n - count;
         
     }
